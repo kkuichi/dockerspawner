05fc73bedfa105fd2ae5b04ce3b715cc7f64b9de
peterbednar <peterbednar@users.noreply.github.com>
2020-11-18 21:40:00 +0100

    Run tests in Docker Swarm environment

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,11 @@
 """Define fixtures for SwarmSpawner tests."""
+
 import pytest
 from unittest.mock import patch
 from jupyterhub.tests.mocking import MockHub
 from jupyterhub.tests.conftest import app, io_loop, event_loop, ssl_tmpdir
-from docker import from_env
+from docker import DockerClient
+from docker.errors import APIError
 
 from dockerspawner import SwarmSpawner
 
@@ -12,10 +14,10 @@ MockHub.hub_ip = "0.0.0.0"
 @pytest.fixture
 def swarmspawner_app(app):
 
-    app.config.SwarmSpawner.name_prefix = "jupyterhub-test"
+    app.config.SwarmSpawner.name_prefix = "jupyterhub-client"
     app.config.SwarmSpawner.default_config = {
         "image": "jupyterhub/singleuser:latest",
-        "networks": ["bridge"]
+        "networks": ["dockerspawner-test-network"]
     }
 
     with patch.dict(app.tornado_settings, {
@@ -27,10 +29,19 @@ def swarmspawner_app(app):
 
 @pytest.fixture(autouse=True, scope="session")
 def docker():
-    client = from_env()
+    client = DockerClient("unix:///var/run/docker.sock")
+
+    client.swarm.init()
+    network = client.networks.create("dockerspawner-test-network", driver="overlay", attachable=True)
+    network.connect("dockerspawner-test")
+
     try:
         yield
     finally:
         for service in client.services.list():
-            if service.name.startswith("jupyterhub-test"):
+            if service.name.startswith("jupyterhub-client"):
                 service.remove()
+
+        network.disconnect("dockerspawner-test")
+        network.remove()
+        client.swarm.leave(True)
